39b1500cc0dd4049335e749f325f38b62ccc44a8
Guttula Swathi <swathiguttula639@gmail.com>
2026-07-14 06:25:45 +0530

    Update queries.sql

--- Assignment_1/queries.sql
+++ Assignment_1/queries.sql
@@ -1,1 +1,17 @@
+--SELECT
+-- 1. Display all employee details
+  select*from Employees;
+
+--2.Display only employee names and salaries.
+SELECT EmployeeName, Salary
+FROM Employee;
+  
+--3.Display employee names and departments.
+select emp_name,department from Employees;
+
+--4.Display all employees from the IT department.
+select * from Employees where department='IT';
+
+--5.Display employee names and experience.
+select emp_name,experience from Employees;
 
